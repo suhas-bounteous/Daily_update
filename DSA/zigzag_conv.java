ed384b2ffc8b5472d9ae693a269be0f4fe3682c4
Suhas S <suhas.s@bounteous.com>
2026-02-24 12:06:23 +0530

    Work done on 23-02-2026

---
+++ DSA/zigzag_conv.java
@@ -0,0 +1,43 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class zigzag_conv {
+    @SuppressWarnings("unchecked")
+    public String convert(String s, int numRows) {
+       if (numRows == 1 || numRows >= s.length()) {
+            return s;
+        }
+
+        int idx = 0, d = 1;
+        List<Character>[] rows = new ArrayList[numRows];
+        for (int i = 0; i < numRows; i++) {
+            rows[i] = new ArrayList<>();
+        }
+
+        for (char c : s.toCharArray()) {
+            rows[idx].add(c);
+            if (idx == 0) {
+                d = 1;
+            } else if (idx == numRows - 1) {
+                d = -1;
+            }
+            idx += d;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (List<Character> row : rows) {
+            for (char c : row) {
+                result.append(c);
+            }
+        }
+
+        return result.toString();        
+    }
+    public static void main(String[] args) {
+        zigzag_conv z = new zigzag_conv();
+        String s = "PAYPALISHIRING";
+        int numRows = 3;
+        String res = z.convert(s, numRows);
+        System.out.println(res);
+    }
+}
---
+++ Daily_lectures/23-03-2026/DynamicArray.java
@@ -0,0 +1,59 @@
+class DynamicArray<T>{
+    private int size;
+    private static final int max = 10;
+    private Object[] data;
+
+    public DynamicArray(){
+        data = new Object[max];
+        size = 0;
+    }
+
+    public void push(T val){
+        ensureSize();
+        data[size++] = val;
+    }
+
+    public void pop(){
+        if(size==0){
+            return;
+        }
+        data[size-1] = null;
+        size--;
+        return;
+    }
+
+    public T peek(){
+        if(size==0){
+            return null;
+        }
+        return (T) data[size-1];
+    }
+
+    public void ensureSize(){
+        if(size == data.length){
+            Object[] newdata = new Object[data.length*2];
+            for(int i=0; i< data.length; i++){
+                newdata[i] = data[i];
+            }
+            data=newdata;
+        }
+    }
+
+    public int size(){
+        return size;
+    }
+    public static void main(String[] args) {
+
+        DynamicArray<String> list = new DynamicArray<>();
+
+        list.push("Apple");
+        list.push("Banana");
+        list.push("Mango");
+
+        System.out.println(list.peek());
+        list.pop();
+        System.out.println(list.peek());
+
+        System.out.println(list.size());
+    }
+}
