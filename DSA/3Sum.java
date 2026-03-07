542f7f08001d0f7a7493bae91ff66e28eddaec6d
Suhas S <suhas.s@bounteous.com>
2026-03-07 22:57:45 +0530

    DSA on 7-3-2026

---
+++ DSA/3Sum.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+class ThreeSum {
+    public List<List<Integer>> threSum(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) {
+                continue;
+            }
+            
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+                int total = nums[i] + nums[j] + nums[k];
+
+                if (total > 0) {
+                    k--;
+                } else if (total < 0) {
+                    j++;
+                } else {
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+
+                    while (nums[j] == nums[j-1] && j < k) {
+                        j++;
+                    }
+                }
+            }
+        }
+        return res;        
+    }
+    
+    public static void main(String[] args) {
+        ThreeSum sol = new ThreeSum();
+        int[] nums = {-1, 0, 1, 2, -1, -4};
+        List<List<Integer>> result = sol.threSum(nums);
+        System.out.println(result);
+    }
+}
