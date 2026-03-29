afa1287270f67db419d973b8da73f02b8e1c69a0
Suhas S <suhas.s@bounteous.com>
2026-03-29 23:57:11 +0530

    Work on 29-03-2026

--- DSA/SearchRotSortArr.java
+++ DSA/SearchRotSortArr.java
@@ -27,4 +27,26 @@ class SearchRotSortArr {
 
         return -1;        
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size of the array: ");
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < n; i++) {
+            nums[i] = sc.nextInt();
+        }
+        System.out.print("Enter the target element to search: ");
+        int target = sc.nextInt();
+        sc.close();
+
+        SearchRotSortArr searcher = new SearchRotSortArr();
+        int result = searcher.search(nums, target);
+        if (result != -1) {
+            System.out.println("Element found at index: " + result);
+        } else {
+            System.out.println("Element not found in the array.");
+        }
+    }
 }
