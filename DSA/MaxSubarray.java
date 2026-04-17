0a0947a964a57cd71e109bf24fa4e44f2c7ac43e
Suhas S <suhas.s@bounteous.com>
2026-04-17 23:27:44 +0530

    d

--- DSA/MaxSubarray.java
+++ DSA/MaxSubarray.java
@@ -3,15 +3,15 @@ import java.util.Arrays;
 public class MaxSubarray {
 
     public static int maxSubArray(int[] nums) {
-        // Initialize with the first element
+        
         int maxSoFar = nums[0];
         int currentMax = nums[0];
 
         for (int i = 1; i < nums.length; i++) {
-            // Decision: Start a new subarray at i, or extend the existing one?
+            
             currentMax = Math.max(nums[i], currentMax + nums[i]);
             
-            // Update the global maximum if the current streak is better
+            
             maxSoFar = Math.max(maxSoFar, currentMax);
         }
 
